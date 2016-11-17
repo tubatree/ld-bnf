b2ee3accaaa1d134ad7bcee3349d28dab4bf87a9
Imran Azad <mohammedimranazad@gmail.com>
2016-11-17 14:35:50 +0000

    Refactor and take into account ordering within nested blank nodes

--- src/ldbnf/order.fs
+++ src/ldbnf/order.fs
@@ -43,75 +43,56 @@ module Order =
                      | _ -> ())
      count
 
-   let addOrderDataProperty id c s =
-       let xs = ref List.Empty
-       s
-       |> List.map(fun s ->
-             match s with
-             | (FSharp.RDF.P p, O(Node.Uri(o), xr)) ->
-               (if o.ToString().Contains(id) = true
-               then
-               xs := List.append !xs [dataProperty !!"nicebnf:hasOrder" (c.ToString()^^xsd.string)])
-               (FSharp.RDF.P p, O(Node.Uri(o), xr))
-             | _ -> s)
-       |> List.append xs.Value
-
-   let addOrderToBlankNode x =
-       let count = ref 0
-       x
-       |> List.map (fun s -> 
-                     match s with
-                     | FSharp.RDF.P(pUri), O(Node.Blank(Blank.Blank(statements)),_) -> 
-                          (count := !count + 1) 
-                          FSharp.RDF.P(pUri), O(Node.Blank(Blank.Blank(statements |> addOrderDataProperty "" count.Value)), lazy [])
-                     | _ -> s)
-
    let addOrderNode (o, p, c) =
     blank !!(p.ToString() + "Order")
            (optionlist {
             yield dataProperty !!"nicebnf:hasOrder" (c.ToString()^^xsd.string)
             yield dataProperty !!(p.ToString()) (o.ToString()^^xsd.string)
             })
-   let addOrderToNestedResources xs x:List<Statement> =
+
+   let addOrderToResources id xs x:List<Statement> =
         let count = ref 0
         x
         |> List.map (fun s -> 
                       match s with
                       | (FSharp.RDF.P p, O(Node.Uri(o), xr)) -> 
                            (count := !count + 1) 
-                           (if (isEligibleForOrder x p).Value > 1 && p.ToString() <> "rdf:type"
+                           if (isEligibleForOrder x p).Value > 1 && p.ToString() <> "rdf:type"
+                             && o.ToString().Contains(id) = false
                            then
-                            xs := List.append !xs [addOrderNode(o, p, count.Value)])
-                           (FSharp.RDF.P p, O(Node.Uri(o), xr))
+                            xs := List.append !xs [addOrderNode(o, p, count.Value)]
+                            (FSharp.RDF.P p, O(Node.Uri(o), xr))
+                           elif (isEligibleForOrder x p).Value > 1 && p.ToString() <> "rdf:type"
+                               && o.ToString().Contains(id) = true
+                           then
+                            let newXr = xr.Value 
+                                        |> List.map(fun s-> match s with
+                                              R(s, statements) -> 
+                                              (count := !count + 1) 
+                                              R(s, statements |> List.append [dataProperty !!"nicebnf:hasOrder" (count.Value.ToString()^^xsd.string)]))
+                            (FSharp.RDF.P p, O(Node.Uri(o), lazy newXr))
+                           else
+                            (FSharp.RDF.P p, O(Node.Uri(o), xr))
                       | _ -> s)
+
+   let rec addOrderToResourcesWithinBlankNodes id s = match s with
+                      | FSharp.RDF.P(pUri), O(Node.Blank(Blank.Blank(statements)),_) -> 
+                        let xs = ref List.Empty
+                        let x = statements 
+                                |> addOrderToResources id xs 
+                                |> List.append xs.Value 
+                                |> List.map (addOrderToResourcesWithinBlankNodes id)
+                        FSharp.RDF.P(pUri), O(Node.Blank(Blank.Blank(x)), lazy [])
+                      | _ -> s
+
    let addOrder id resources =
-     let parentResources = ref List.Empty
-     let count = ref 0
+     let xs = ref List.Empty
      let id = id.ToString()
      resources
      |> List.map(fun r ->
          match r with
          | R(s, statements) ->
-           R(s, statements 
-                |> List.map (fun s ->
-                     let nestedResource = ref List.Empty
-                     match s with
-                     | (FSharp.RDF.P p, O(Node.Uri(o), resources)) -> 
-                       (count := !count + 1) 
-                       (if (isEligibleForOrder statements p).Value > 1 
-                           && p.ToString() <> "rdf:type" && o.ToString().Contains(id) = false
-                       then
-                        parentResources := List.append !parentResources [addOrderNode(o, p, !count)])
-                       let newRes = 
-                         resources.Value
-                         |> List.map(fun r -> 
-                                      match r with
-                                      | R(s, statements) -> 
-                                           R(s, statements
-                                                |> addOrderToBlankNode
-                                                |> addOrderToNestedResources nestedResource
-                                                |> List.append nestedResource.Value
-                                                |> (fun c-> nestedResource := List.empty; c)
-                                                |> addOrderDataProperty id count.Value))
-                       (FSharp.RDF.P p, O(Node.Uri(o), lazy newRes))
-                     | _ -> s) |> List.append parentResources.Value |> (fun c -> parentResources:= List.Empty; c)))
+           R(s, statements |> addOrderToResources id xs
+                           |> List.append !xs 
+                           |> (fun s -> xs:= List.Empty; s)
+                           |> List.map (addOrderToResourcesWithinBlankNodes id)))
